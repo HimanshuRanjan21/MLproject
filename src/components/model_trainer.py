3b9e546b846b192d69d8d93b1d7eca7da2b05740
Himanshu <20ec3021@rgipt.ac.in>
2023-12-30 13:14:01 +0530

    model_trainer0.1

---
+++ src/components/model_trainer.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from dataclasses import dataclass
+from src.exception import CustomException
+from src.logger import logging
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import(
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeClassifier
+
+
+
