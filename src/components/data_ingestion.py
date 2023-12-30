8999119eb1a329baf266cad7bbf72f9a46cee244
Himanshu <20ec3021@rgipt.ac.in>
2023-12-30 12:14:12 +0530

    Data Transformation & utils

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -46,11 +46,17 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
             
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 if __name__=='__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+
 
 
 
