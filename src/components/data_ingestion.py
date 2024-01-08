a24b9e189455620234ab57eaa24f37aa88938ae4
Himanshu <20ec3021@rgipt.ac.in>
2024-01-08 22:45:15 +0530

    Modle_trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -48,13 +48,18 @@ class DataIngestion:
             
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 if __name__=='__main__':
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
 
 
 
--- src/exception.py
+++ src/exception.py
@@ -17,10 +17,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__=='__main__':
-    try:
-        a=1/0
-    except Exception as e:
+# if __name__=='__main__':
+#     try:
+#         a=1/0
+#     except Exception as e:
 
-        logging.info('Divide_by_zero')
-        raise CustomException(e,sys)
+#         logging.info('Divide_by_zero')
+#         raise CustomException(e,sys)
